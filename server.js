6d4064d3bcd0b096af57c86bbd7337f319bedcdb
Pedro Henrique <61064109+PedroDoliwa@users.noreply.github.com>
2025-10-31 17:33:01 -0300

    CRUD completo com Express

--- server.js
+++ server.js
@@ -1,18 +1,7 @@
-import http from 'http';
+import app from './src/app.js';
 
 const PORT = 3000;
 
-const rotas = {
-    '/': "Curso de Node.js",
-    '/livros': "Você está na página de livros",
-    '/autores': "Você está na página de autores",
-};
-
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end(rotas[req.url]);
-});
-
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log('servidor escutando!');
 });
